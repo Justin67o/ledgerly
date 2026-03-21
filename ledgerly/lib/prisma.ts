924914dbd7a21ed11a164362433c2fd4ac8e3bcc
Justin <jg3chen@uwaterloo.ca>
2026-03-21 19:44:03 -0400

    fix file structure

--- ledgerly/lib/prisma.ts
+++
@@ -1,6 +0,0 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
-
-// Create a single PrismaClient instance
-// This is what your app and test scripts will import
-export const prisma = new PrismaClient();
